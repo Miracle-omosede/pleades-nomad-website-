e285f65cac4eec36391ffce65379514b8165333e
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-03-20 20:13:29 +0100

    first commit

---
+++ src/components/Navbar/NavUtils/Navlist2.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+type Props = [
+    main: String
+    path: string
+]
+
+const Navlist2 = ({main ,path}: props) => {
+  return (
+    <div>Navlist2</div>
+  )
+}
+
+export default Navlist2
